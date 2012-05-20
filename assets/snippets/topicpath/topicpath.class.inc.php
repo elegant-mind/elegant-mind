b34140fdad13e95e95929d426fdd64086543fe4d
yama <yamamoto@kyms.ne.jp>
2012-05-20 22:53:56 +0900

    Fix - TopicPath snippet

--- assets/snippets/topicpath/topicpath.class.inc.php
+++ assets/snippets/topicpath/topicpath.class.inc.php
@@ -1,5 +1,5 @@
 <?php
-$class_version = '1.0.2';
+$class_version = '1.0.3';
 if(!isset($version) || $version !== $class_version) echo 'TopicPath version error';
 
 class TopicPath
@@ -112,7 +112,7 @@ class TopicPath
 		* published, hidemenu
 		*/
 		$topics = array();
-		$parent = &$modx->documentObject['parent'];
+		$parent = $modx->documentObject['parent'];
 		$output = '';
 		$display += ($showCurrentTopic) ? 1 : 0;
 		
@@ -124,7 +124,7 @@ class TopicPath
 		// Decide if current page is to be a topic
 		if ( $showCurrentTopic )
 		{
-			$topics[] = &$modx->documentObject;
+			$topics[] = $modx->documentObject;
 		}
 		
 		// Intermediate topics ---------------------------------------------------------
