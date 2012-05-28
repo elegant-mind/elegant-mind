4a037406330527544c2a7fd73f6653f1f1881fa3
sjstoelting <mail@stefanie-stoelting.de>
2012-05-28 11:47:20 +0200

    Added a script, to run all unit tests from command line, *NIX only, of course.

---
+++ _Test/unit_tests/runUnitTests.sh
@@ -0,0 +1,5 @@
+# Run all unit tests
+
+phpunit -c _Test/unit_tests/phpunit.xml _Test/unit_tests/manager/includes/DocumentParserTest.php
+
+phpunit -c _Test/unit_tests/phpunit.xml _Test/unit_tests/manager/includes/SystemEventTest.php
