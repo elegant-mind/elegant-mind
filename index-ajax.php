d7f03f693e20315dc385dc28ffb0136f6a0583fc
yama <yamamoto@kyms.ne.jp>
2012-06-21 12:18:27 +0900

    Fix - index-ajax.php

--- index-ajax.php
+++ index-ajax.php
@@ -3,9 +3,11 @@ if(isset($_GET['q']) && $_GET['q']!=='')       $q = $_GET['q'];
 elseif(isset($_POST['q']) && $_POST['q']!=='') $q = $_POST['q'];
 else exit;
 
-$q = realpath($q) or die(); 
-
 define('MODX_API_MODE', true);
 include_once('index.php');
+$q = realpath($q) or die();
+$q = str_replace('\\','/',$q);
 
-if(strtolower(substr($q,-4))=='.php') include_once($q);
+if(strpos($q, MODX_BASE_PATH . 'assets/snippets/')!==0) exit;
+if(strtolower(substr($q,-4))!=='.php') exit;
+include_once($q);
