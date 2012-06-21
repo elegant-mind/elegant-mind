84af661fd5d8c4484e34f47eca9362a388d15a3f
yama <yamamoto@kyms.ne.jp>
2012-06-21 12:27:50 +0900

    Process order clearly

--- index-ajax.php
+++ index-ajax.php
@@ -3,10 +3,11 @@ if(isset($_GET['q']) && $_GET['q']!=='')       $q = $_GET['q'];
 elseif(isset($_POST['q']) && $_POST['q']!=='') $q = $_POST['q'];
 else exit;
 
-define('MODX_API_MODE', true);
-include_once('index.php');
 $q = realpath($q) or die();
 $q = str_replace('\\','/',$q);
+
+define('MODX_API_MODE', true);
+include_once('index.php');
 
 if(strpos($q, MODX_BASE_PATH . 'assets/snippets/')!==0) exit;
 if(strtolower(substr($q,-4))!=='.php') exit;
